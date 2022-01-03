071052706a700f552e32efa06e21fefac2de2bef
Ahmed shafik <a.mahmuod@gmail.com>
2022-01-03 12:30:11 +0200

    only parse the required data

--- MoveDB/Classes/Home/Models/MoviesDTO.swift
+++ MoveDB/Classes/Home/Models/MoviesDTO.swift
@@ -23,35 +23,14 @@ struct MoviesDTO: Codable {
 }
 // MARK: - Result
 struct MovieDataDTO: Codable {
-    let adult: Bool
-    let backdropPath: String
-    let genreIDS: [Int]
     let id: Int
-    let originalLanguage: OriginalLanguage
-    let originalTitle, overview: String
-    let popularity: Double
-    let posterPath, releaseDate, title: String
-    let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+    let title: String
+    let posterPath : String
 
     enum CodingKeys: String, CodingKey {
-        case adult
-        case backdropPath = "backdrop_path"
-        case genreIDS = "genre_ids"
         case id
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case overview, popularity
         case posterPath = "poster_path"
-        case releaseDate = "release_date"
-        case title, video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
+        case title
     }
 }
 
-enum OriginalLanguage: String, Codable {
-    case en = "en"
-    case hi = "hi"
-}
